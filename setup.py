ebca94fdf0db11f4101d6e827a2be223da51c248
Rik Bose <rik@buoy.org>
2017-04-27 01:18:47 -0400

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools.command.install import install
 if __name__ == '__main__':
     setup(
             name='genesis',
-            version='0.1.0',
+            version='0.1.1',
             packages=find_packages(exclude=['test']),
             install_requires=[
                 'spacy==1.7.0',

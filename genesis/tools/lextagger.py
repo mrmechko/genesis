baf4c4ce42535e58c6d1cade146a4e3497c9c3a3
Ritwik Bose <mrmechko@gmail.com>
2018-10-09 16:31:17 -0400

    big cache

--- genesis/tools/lextagger.py
+++ genesis/tools/lextagger.py
@@ -9,6 +9,7 @@ sw_en = set(stopwords.words('english'))
 
 CACHE_WORDNET = defaultdict(lambda: defaultdict(list))
 CACHE_LEXICON = defaultdict(lambda: defaultdict(list))
+BIG_CACHE = defaultdict(lambda: defaultdict(list))
 
 
 def normalize_spacy_to_trips(token):
@@ -96,13 +97,28 @@ def lookup_all(token, types_only=False, wndepth=3):
         return set(lex + wnl)
     return lex, wnl
 
+
+def extract_types(res):
+    """WN are returned as [([types], [wn]) ...]"""
+    lex, wn = res
+    wn = [r[0] for r in wn]
+    res = set(lex)
+    for w in wn:
+        res.update(w)
+    return res
+
+
 def lookup_types(token, pos=None, wndepth=3):
     if not pos:
         pos = get_pos(token, deep_syntax(token))
     token = normalize_spacy_to_trips(token.text.lower())
     if pos:
         pos = pos[0]
-        return (lookup_lexicon_type(token, pos), lookup_wordnet_type(token, pos))
+        if token not in BIG_CACHE[pos]:
+            res = (lookup_lexicon_type(token, pos), lookup_wordnet_type(token, pos))
+            BIG_CACHE[pos][token] = extract_types(res)
+        return BIG_CACHE[pos][token][:]
+    return []
 
 def lookup_types_sentence(sentence):
     sentence = nlp(sentence)
